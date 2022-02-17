dce209e82b87890f101200f7828df34d1e568272
wwhymadd <99489845+wwhymadd@users.noreply.github.com>
2022-02-17 22:55:28 +0300

    Update solution_test.go

--- solution_test.go
+++ solution_test.go
@@ -1,1 +1,13 @@
 package square
+
+import (
+	"testing"
+)
+
+func TestCalcSquare(t *testing.T) {
+	var expected = 43.30127018922193
+	if CalcSquare() != expected {
+		t.Errorf("Ошибка. Expect %v, got %v",
+			expected, CalcSquare())
+	}
+}
